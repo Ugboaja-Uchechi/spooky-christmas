f3df3f48a28848e19f02944dfb6fed5a2dd55877
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-02 16:27:55 +0100

    Add javascript content

---
+++ index.js
@@ -0,0 +1,7 @@
+const greeting = document.getElementById("greeting");
+const fright = document.getElementById("fright");
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+  document.body.classList.add('christmas');
+});
